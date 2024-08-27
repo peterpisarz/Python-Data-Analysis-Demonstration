0f3ff0116f809a260dda163287b15a0c63a3815d
Peter Pisarz <peter.pisarz@gmail.com>
2024-08-27 14:50:53 -0500

    Updating PORT

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def update_graph(selected_run, selected_params):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
